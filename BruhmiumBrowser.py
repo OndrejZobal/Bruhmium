6e16ab5c61d26092afa8d7de57a7211565303d67
OndrejZobal <40278421+OndrejZobal@users.noreply.github.com>
2019-09-30 16:31:57 +0200

    0.0.0
    Bruh

---
+++ BruhmiumBrowser.py
@@ -0,0 +1,19 @@
+import tkinter
+import BruhmiumCore
+
+
+def main():
+    pass
+
+
+def debug():
+    pass
+
+
+if __name__ == '__main__':
+    print('THIS SCRIPT IS NOT TO BE RAN ALONE \nDo you wish to continue in debug mode?\n')
+    if input(">") in ['yes', 'y', '1']:
+        debug()
+    input('Press any key to continue...')
+else:
+    print('Bruhmium-Browser modul loaded')
